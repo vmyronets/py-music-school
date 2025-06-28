a897548789784aaa6444ad763108e2510c7b503d
Volodymyr Myronets <volodymyrmyronets@gmail.com>
2025-06-28 12:51:39 +0300

    Add basename to router in file musician/urls.py

--- musician/urls.py
+++ musician/urls.py
@@ -7,7 +7,7 @@ from musician.views import MusicianViewSet
 
 router = routers.DefaultRouter()
 
-router.register("musician", MusicianViewSet)
+router.register("musician", MusicianViewSet, basename="manage")
 urlpatterns = [
     path("", include(router.urls)),
 ]
